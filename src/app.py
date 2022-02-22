7acefd8226df57ee4a585e4a0cd43b10f0329667
ins4w <amejia.foo@gmail.com>
2022-02-22 17:03:00 -0500

    fix: issues with words fetching

--- src/app.py
+++ src/app.py
@@ -1,1 +1,23 @@
 from colorama import Fore
+import sys, platform, os
+from words import getSpanishWords
+
+def main():
+
+    getSpanishWords(5, "palabras5.txt")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
+"""
+if len(sys.argv) != 2:
+    message = "Usage: python app.py <words length>"
+    if platform.system() != "Windows":
+        print(message.replace("python", "python3"))
+    else:
+        print(message)
+    sys.exit(1)
+
+words_length = sys.argv[1]
+"""
--- src/words.py
+++ src/words.py
@@ -1,13 +1,16 @@
-from requests import get
+import requests
 from bs4 import BeautifulSoup
 
-def getWordsOfLength(length):
-    page = get("https://github.com/javierarce/palabras/blob/master/listado-general.txt")
+# returns an array of words of length n or writes words of length n to a file
+def getSpanishWords(words_length, outfile_name = None):
+    page = requests.get("https://github.com/javierarce/palabras/blob/master/listado-general.txt")
     soup = BeautifulSoup(page.text, 'lxml')
-    words_tags = soup.find_all('td', class_='blob-code')
-    with open(f'{length}LengthWords.txt', 'w') as file:
-        for word_tag in words_tags:
-            word = f'{word_tag.text}\n'
-            if len(word) == length : file.write(word)
-
-getWordsOfLength(8)
+    words_tags = soup.find_all('td', class_ = 'blob-code')
+    words = list(map(lambda word_tag: word_tag.text, words_tags))
+    if outfile_name != None:
+        with open(f"./words/{outfile_name}", 'w') as outfile:
+            for word in words:
+                if len(word) == words_length:
+                    outfile.write(word + '\n')
+    else:
+        return list(filter(lambda word: len(word) == words_length, words))
