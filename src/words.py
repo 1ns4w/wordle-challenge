15f4a0b31b4f1ef21c23796d57f81f7a263842a9
ins4w <amejia.foo@gmail.com>
2022-02-22 12:53:09 -0500

    feat: add support for requesting  n length words

--- src/words.py
+++ src/words.py
@@ -1,12 +1,13 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-def getWords():
+def getWordsOfLength(length):
     page = get("https://github.com/javierarce/palabras/blob/master/listado-general.txt")
     soup = BeautifulSoup(page.text, 'lxml')
-    words = soup.find_all('td', class_='blob-code')
-    for word in words:
-        if len(word) == 5:
-            print(word)
+    words_tags = soup.find_all('td', class_='blob-code')
+    with open(f'{length}LengthWords.txt', 'w') as file:
+        for word_tag in words_tags:
+            word = f'{word_tag.text}\n'
+            if len(word) == length : file.write(word)
 
-getWords()
+getWordsOfLength(8)
