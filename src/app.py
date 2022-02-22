b88e286887f389966867f27f17d708d6e22154ad
ins4w <amejia.foo@gmail.com>
2022-02-22 11:49:30 -0500

    fix: access to page dom

--- src/app.py
+++ src/app.py
@@ -1,19 +1,15 @@
 from colorama import Fore
-from requests_html import HTMLSession
+import requests
+from bs4 import BeautifulSoup
 
-def get5LengthWords():
-    session = HTMLSession()
-    page = session.get("https://github.com/javierarce/palabras/blob/master/listado-general.txt")
-    print(page.status_code )
 
-def normalizeVowel(vowel):
-    vowels = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u'}
-    if vowel.lower() not in vowels:
-        return vowel
-    return vowels[vowel.lower()].upper() if vowel.isupper() else vowels[vowel]
+def getWords():
+    page_request = requests.get("https://github.com/javierarce/palabras/blob/master/listado-general.txt")
+    page_dom = page_request.text
+    soup = BeautifulSoup(page_dom, 'lxml')
+    tags = soup.find('div')
+    print(tags.prettify())
 
-print(normalizeVowel('á'))
 
-word = 'REINA'
 
-print(get5LengthWords())
+print(getWords())
