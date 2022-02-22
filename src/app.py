21bbe4dffe4e86178ae7dcde6a4233965a8eb3f5
ins4w <amejia.foo@gmail.com>
2022-02-22 09:14:34 -0500

    chore: solicitar contenido de archivo con palabras en gh

--- src/app.py
+++ src/app.py
@@ -1,3 +1,11 @@
+from colorama import Fore
+import requests
+
+def getSomeLengthWords(lenght):
+    page = requests.get("https://github.com/javierarce/palabras/blob/master/listado-general.txt")
+    page_content = page.text
+    print(page_content)
+
 def normalizeVowel(vowel):
     vowels = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u'}
     if vowel.lower() not in vowels:
@@ -7,3 +15,5 @@ def normalizeVowel(vowel):
 print(normalizeVowel('á'))
 
 word = 'REINA'
+
+print(getSomeLengthWords(2))
