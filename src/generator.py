a83f0b771dfb6d7319441e485ea27c15360d98a7
ins4w <amejia.foo@gmail.com>
2022-02-25 09:14:29 -0500

    fix: missing dep

--- src/generator.py
+++ src/generator.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # writes to a file or returns an array of n words of length m
-def getSpanishWords(words_length, outfile_name = None, total_words = None):
+def getSpanishWords(words_length, outfile_path = None, total_words = None):
 
     page = requests.get("https://github.com/javierarce/palabras/blob/master/listado-general.txt")
     soup = BeautifulSoup(page.text, 'lxml')
@@ -11,8 +11,8 @@ def getSpanishWords(words_length, outfile_name = None, total_words = None):
     words_tags_text = list(map(lambda word_tag: word_tag.text, words_tags))
     words = list(filter(lambda word: len(word) == words_length, words_tags_text))
 
-    if outfile_name != None:
-        with open(outfile_name, 'w') as outfile:
+    if outfile_path != None:
+        with open(outfile_path, 'w') as outfile:
             for index in range(len(words) if total_words == None or total_words > len(words) else total_words):
                 outfile.write(words[index] + '\n')
     else:
