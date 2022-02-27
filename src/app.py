16f30270096e932f691e55f852a4ed4bd098cc10
ins4w <amejia.foo@gmail.com>
2022-02-26 20:06:18 -0500

    feat: support for existing file with words

--- src/app.py
+++ src/app.py
@@ -1,21 +1,29 @@
+from os import path, stat
 from datetime import datetime
+from colorama import Back, Fore, init
 from generator import getSpanishWords
-from colorama import Back, Fore
-from helpers import clear, normalize_words, print_colored_grid, style_text
-from constants import GAME_ATTEMPTS, WORDS_LENGTH, TOTAL_WORDS, HASH_SUBSTRACTION
+from constants import GAME_ATTEMPTS, WORDS_LENGTH, TOTAL_WORDS, HASH_SUBSTRACTION, INFILE_PATH
+from helpers import clear, normalize_words, print_colored_grid, style_text, lineBreakSeparatedValuesToArray
 
 def main():
 
+    init()
     clear()
-    print(f"Cargando...")
-    words = normalize_words(getSpanishWords(WORDS_LENGTH))
-    game_words = words[:TOTAL_WORDS]
+
+    if path.exists(INFILE_PATH) == False or stat(INFILE_PATH).st_size == 0:
+        print(f"Cargando...")
+        words = normalize_words(getSpanishWords(WORDS_LENGTH))
+    else:
+        infile_text = open(INFILE_PATH, 'r').read()
+        words = normalize_words(lineBreakSeparatedValuesToArray(infile_text))
 
     game_grid = []
     attempts_counter = 0
+    game_words = words[:TOTAL_WORDS]
+
+    clear()
     day_of_year = int(datetime.now().strftime('%j')) - HASH_SUBSTRACTION
     day_word = game_words[day_of_year - 1]
-    clear()
 
     while attempts_counter < GAME_ATTEMPTS:
 
@@ -40,13 +48,14 @@ def main():
         
         for i in range(len(day_word)):
             if answer_chars[i] == day_word[i]:
-                answer_chars[i] = style_text(Back.GREEN, Fore.BLACK, answer_chars[i])
+                answer_chars[i] = style_text(answer_chars[i], Back.GREEN, Fore.BLACK)
             elif answer_chars[i] in day_word:
-                answer_chars[i] = style_text(Back.YELLOW, Fore.BLACK, answer_chars[i])
+                answer_chars[i] = style_text(answer_chars[i], Back.YELLOW, Fore.BLACK)
             else:
-                answer_chars[i] = style_text(Back.WHITE, Fore.BLACK, answer_chars[i])
+                answer_chars[i] = style_text(answer_chars[i], Back.WHITE, Fore.BLACK)
 
         game_grid.append(answer_chars)
+        print_colored_grid(game_grid)
 
         if answer == day_word:
             print("Ganaste.")
